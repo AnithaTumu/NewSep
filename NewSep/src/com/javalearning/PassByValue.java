0c2ea757743f14cfaa6abd97b42d348b16630333
Anitha <anithatumu401@gmail.com>
2023-09-19 22:00:53 +0530

    dev changes to add hashcode

--- NewSep/src/com/javalearning/PassByValue.java
+++ NewSep/src/com/javalearning/PassByValue.java
@@ -16,6 +16,7 @@ public class PassByValue {
 	public void passByReference(PassByValue ref)
 	{
 		//Method2
+		System.out.println("hashcode: " + ref.hashCode());
 		System.out.println("Initial Value of a: " + ref.a);
 		ref.a = ref.a+10;
 		System.out.println("Final Value of a: " + ref.a);
@@ -25,10 +26,12 @@ public class PassByValue {
 		PassByValue passByValue = new PassByValue();
 		passByValue.a = 20;
 		System.out.println("Before passByValue method a: " + passByValue.a);
+		System.out.println("hashcode: " + passByValue.hashCode());
 		passByValue.passByValue(passByValue.a);
 		System.out.println("After passByValue method of a: " + passByValue.a);	
 		
 		System.out.println("Before passByReference method a: " + passByValue.a);
+		System.out.println("hashcode: " + passByValue.hashCode());
 		passByValue.passByReference(passByValue);
 		System.out.println("After passByReference method of a: " + passByValue.a);	
 
